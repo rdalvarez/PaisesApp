a5af90cfb355df047ea112c95324c2c909576110
rdalvarez <david_de_odin@hotmail.com>
2021-08-15 12:19:16 -0300

    Llenar tabla de países

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PaisService } from '../../services/pais.service';
+import { Country } from '../../interfaces/pais.interface';
 
 @Component({
   selector: 'app-por-pais',
@@ -11,6 +12,7 @@ export class PorPaisComponent {
 
   termino: string= 'Hola Mundo';
   hayError: boolean = false;
+  paises : Country[] = [];
 
   constructor(private pais$: PaisService) { }
 
@@ -21,9 +23,10 @@ export class PorPaisComponent {
     this.pais$.buscarPais( this.termino )
       .subscribe( (paises) => {
         console.log(paises);
-        
+        this.paises = paises;
       }, ( err ) => {
         this.hayError = true;
+        this.paises = [];
       } ) ;
   }
 }
