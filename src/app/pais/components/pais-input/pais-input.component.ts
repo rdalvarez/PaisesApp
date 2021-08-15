1d96b303042c7891a671aacf66ef69b294121090
rdalvarez <david_de_odin@hotmail.com>
2021-08-15 17:54:21 -0300

    Componente Input y Tabla

---
+++ src/app/pais/components/pais-input/pais-input.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pais-input',
+  templateUrl: './pais-input.component.html',
+  styles: [],
+})
+export class PaisInputComponent {
+  termino: string = '';
+  buscar() {
+    console.log(this.termino);
+  }
+}
