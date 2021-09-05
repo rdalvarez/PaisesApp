204b993fe600490b37c1785acd075199b32ad86b
rdalvarez <david_de_odin@hotmail.com>
2021-09-05 16:27:23 -0300

    RxJs - SwitchMap

--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { switchMap } from "rxjs/operators";
+
 import { PaisService } from '../../services/pais.service';
 
 @Component({
@@ -18,13 +20,21 @@ export class VerPaisComponent implements OnInit {
   ngOnInit(): void {
 
     this.activatedRoute.params
-      .subscribe ( ({ id }) => {
-        console.log(id);
-        this.pais$.getPaisPorAlpha(id)
-          .subscribe( (pais) => {
-            console.log(pais);
-          })
-      })
+    .pipe(
+      switchMap((param) => this.pais$.getPaisPorAlpha(param.id))
+    )
+    .subscribe( (resp) =>{
+      console.log(resp);
+    })
+
+    // this.activatedRoute.params
+    //   .subscribe ( ({ id }) => {
+    //     console.log(id);
+    //     this.pais$.getPaisPorAlpha(id)
+    //       .subscribe( (pais) => {
+    //         console.log(pais);
+    //       })
+    //   })
 
   }
 
