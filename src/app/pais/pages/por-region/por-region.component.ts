d1253a45087585aaa2b6199344d43e933f7a74aa
rdalvarez <david_de_odin@hotmail.com>
2021-09-18 17:29:20 -0300

    ngClass, class y [class]

--- src/app/pais/pages/por-region/por-region.component.ts
+++ src/app/pais/pages/por-region/por-region.component.ts
@@ -1,16 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-por-region',
   templateUrl: './por-region.component.html',
-  styles: [
-  ]
+  styles: [ `
+    button {
+      margin-right: 5px;
+    }
+  ` ]
 })
-export class PorRegionComponent implements OnInit {
+export class PorRegionComponent {
+
+  regiones: Array<string> = ['africa', 'americas', 'asia', 'europe', 'oceania'];
+  regionActiva: string = '';
 
   constructor() { }
-
-  ngOnInit(): void {
+  
+  activarRegion( region: string ){
+    this.regionActiva = region;
+    //TODO: hacer el llamado al servicio
   }
 
 }
