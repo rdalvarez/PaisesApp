631b393a1618a822eef736e49cfd6ccad7e2542b
rdalvarez <david_de_odin@hotmail.com>
2021-09-18 17:39:53 -0300

    Clases de CSS condicionales

--- src/app/pais/pages/por-region/por-region.component.ts
+++ src/app/pais/pages/por-region/por-region.component.ts
@@ -16,6 +16,12 @@ export class PorRegionComponent {
 
   constructor() { }
   
+  getClaseCSS(region: string): string {
+    return ( region === this.regionActiva )
+                     ? 'btn btn-primary'
+                     : 'btn btn-outline-primary';
+  }
+
   activarRegion( region: string ){
     this.regionActiva = region;
     //TODO: hacer el llamado al servicio
